df4bf60ebc36e6e7c2acccf53c1d689fe320c747
MqNC8 <MqNC8@GODOFGAME-V2>
2021-12-22 00:22:37 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            //Açıklama Satırı
+            //değişken tanımla
+            //type isim;
+
+            int a=1, b, c;
+            string ad = "Mutlu";
+            bool kontrol = true;
+            char karakter = 'x';
+            byte sayi = 255;
+            byte sonuc, sayi1=10, sayi2=2;
+            //sonuc = sayi1 + sayi2;
+            double fiyat = 45.99;
+            decimal agirlik = 123.234M;
+            sbyte derece1 = -10;
+
+            Console.WriteLine(ad);
+            Console.WriteLine("ad");
+            
+
+
+
+            
+        }
+    }
+}
